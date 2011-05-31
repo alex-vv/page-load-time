6d9508445470c36c3fa0471cd25b005fee6e383f
Alexander Vykhodtsev <avflance@gmail.com>
2011-05-31 13:50:49 +0700

    function isolation

--- src/timer.js
+++ src/timer.js
@@ -1,11 +1,12 @@
-function check() {
-    (document.readyState == "complete") ? measure() : setTimeout(check, 100);
-};
-check();
-
-function measure() {
-    var t = performance.timing;
-    // we have only 4 chars in our disposal including decimal point
-    var time = String(((t.loadEventEnd - t.fetchStart) / 1000).toPrecision(3)).substring(0, 4);
-    chrome.extension.sendRequest({time: time, timing: t});
-}
+(function() {
+    (function check() {
+        (document.readyState == "complete") ? measure() : setTimeout(check, 100);
+    })();
+    
+    function measure() {
+        var t = performance.timing;
+        // we have only 4 chars in our disposal including decimal point
+        var time = String(((t.loadEventEnd - t.fetchStart) / 1000).toPrecision(3)).substring(0, 4);
+        chrome.extension.sendRequest({time: time, timing: t});
+    }
+})();
