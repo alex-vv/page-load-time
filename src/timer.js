ec7295b6f7091cf800d0cf4715b573817142d0dc
Alexander Vykhodtsev <avflance@gmail.com>
2011-05-31 14:37:11 +0700

    redirect time is also included

--- src/timer.js
+++ src/timer.js
@@ -5,9 +5,10 @@
     
     function measure() {
         var t = performance.timing;
+        var start = t.redirectStart == 0 ? t.fetchStart : t.redirectStart;
         if (t.loadEventEnd > 0) {
             // we have only 4 chars in our disposal including decimal point
-            var time = String(((t.loadEventEnd - t.fetchStart) / 1000).toPrecision(3)).substring(0, 4);
+            var time = String(((t.loadEventEnd - start) / 1000).toPrecision(3)).substring(0, 4);
             chrome.extension.sendRequest({time: time, timing: t});
         }
     }
