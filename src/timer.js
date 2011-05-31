c071656c38ad425709ab9c560513d911de6123aa
Alexander Vykhodtsev <avflance@gmail.com>
2011-05-31 13:54:13 +0700

    sometimes loadEventEnd is 0

--- src/timer.js
+++ src/timer.js
@@ -5,8 +5,10 @@
     
     function measure() {
         var t = performance.timing;
-        // we have only 4 chars in our disposal including decimal point
-        var time = String(((t.loadEventEnd - t.fetchStart) / 1000).toPrecision(3)).substring(0, 4);
-        chrome.extension.sendRequest({time: time, timing: t});
+        if (t.loadEventEnd > 0) {
+            // we have only 4 chars in our disposal including decimal point
+            var time = String(((t.loadEventEnd - t.fetchStart) / 1000).toPrecision(3)).substring(0, 4);
+            chrome.extension.sendRequest({time: time, timing: t});
+        }
     }
 })();
