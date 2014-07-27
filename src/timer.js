b569ccaa34d5d58a257964d060db7c5732b457cb
Alexander Vykhodtsev <avflance@gmail.com>
2014-07-27 17:19:34 +0700

    Refactoring

--- src/timer.js
+++ src/timer.js
@@ -1,16 +1,21 @@
 (function() {
-    (function check() {
-        (document.readyState == "complete") ? measure() : setTimeout(check, 100);
-    })();
+    if (document.readyState == "complete") {
+        measure();
+    } else {
+        window.addEventListener("load", measure);
+    }
     
     function measure() {
-        var t = performance.timing;
-        var start = t.redirectStart == 0 ? t.fetchStart : t.redirectStart;
-        if (t.loadEventEnd > 0) {
-            // we have only 4 chars in our disposal including decimal point
-            var time = String(((t.loadEventEnd - start) / 1000).toPrecision(3)).substring(0, 4);
-            var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';            
-            chrome[roe].sendMessage({time: time, timing: t});
-        }
+        setTimeout(function() {
+            var t = performance.timing;
+            var start = t.redirectStart == 0 ? t.fetchStart : t.redirectStart;
+            if (t.loadEventEnd > 0) {
+                // we have only 4 chars in our disposal including decimal point
+                var time = String(((t.loadEventEnd - start) / 1000).toPrecision(3)).substring(0, 4);
+                var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';            
+                chrome[roe].sendMessage({time: time, timing: t});
+            }
+        }, 0);
     }
 })();
+
