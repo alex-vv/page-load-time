5bc4f3be26ce421a3fb5a8cda8f99c7e50559442
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2019-02-07 15:30:36 +0600

    Fix rounding in the toolbar

--- src/performance.js
+++ src/performance.js
@@ -12,7 +12,7 @@
       const l = performance.getEntriesByType('navigation')[0].toJSON();
       if (l.duration > 0) {
         // we have only 4 chars in our disposal including decimal point
-        var t = String((l.duration / 1000).toPrecision(3)).substring(0, 4);
+        var t = (l.duration / 1000).toFixed(2);
         chrome.runtime.sendMessage({time: t, timing: l});
       }
     }, 0);
