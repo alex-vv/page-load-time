045ab008e2d38fb70b4e570d22a71a1f9c956552
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2020-06-08 00:27:37 +0600

    Add adjustments for Firefox

--- src/performance.js
+++ src/performance.js
@@ -10,6 +10,26 @@
   function startCollect() {
     const timing = performance.getEntriesByType('navigation')[0].toJSON();
     if (timing.duration > 0) {
+      // fetchStart sometimes negative in FF, make an adjustment based on fetchStart
+      var adjustment = timing.fetchStart < 0 ? -timing.fetchStart : 0;
+      ['domainLookupStart',
+        'domainLookupEnd',
+        'connectStart',
+        'connectEnd',
+        'requestStart',
+        'responseStart',
+        'responseEnd',
+        'domComplete',
+        'domInteractive',
+        'domContentLoadedEventStart',
+        'domContentLoadedEventEnd',
+        'loadEventStart',
+        'loadEventEnd',
+        'duration'
+      ].forEach(i => {
+        timing[i] +=adjustment;
+      });
+
       // we have only 4 chars in our disposal including decimal point
       var time = (timing.duration / 1000).toFixed(2);
       browser.runtime.sendMessage({time: time, timing: timing});
