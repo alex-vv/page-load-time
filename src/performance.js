267220b6494bfa8fcc993eeb3f653b9dc0a0eeb5
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2020-06-07 23:39:46 +0600

    Wait for non-zero duration

--- src/performance.js
+++ src/performance.js
@@ -8,13 +8,13 @@
   }
 
   function startCollect() {
-    setTimeout(function() {
-      const l = performance.getEntriesByType('navigation')[0].toJSON();
-      if (l.duration > 0) {
-        // we have only 4 chars in our disposal including decimal point
-        var t = (l.duration / 1000).toFixed(2);
-        chrome.runtime.sendMessage({time: t, timing: l});
-      }
-    }, 0);
+    const timing = performance.getEntriesByType('navigation')[0].toJSON();
+    if (timing.duration > 0) {
+      // we have only 4 chars in our disposal including decimal point
+      var time = (timing.duration / 1000).toFixed(2);
+      chrome.runtime.sendMessage({time: time, timing: timing});
+    } else {
+      setTimeout(startCollect, 100);
+    }
   }
 })();
