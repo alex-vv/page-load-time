049aea0a25e4d24d9b1affc72365fd6c249001be
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2020-06-14 01:56:28 +0600

    Make it work with pages which use `serverTiming`

--- src/performance.js
+++ src/performance.js
@@ -10,6 +10,7 @@
   function startCollect() {
     const timing = performance.getEntriesByType('navigation')[0].toJSON();
     timing.start = performance.timing.requestStart;
+    delete timing.serverTiming;
     if (timing.duration > 0) {
       // fetchStart sometimes negative in FF, make an adjustment based on fetchStart
       var adjustment = timing.fetchStart < 0 ? -timing.fetchStart : 0;
@@ -33,7 +34,8 @@
 
       // we have only 4 chars in our disposal including decimal point
       var time = (timing.duration / 1000).toFixed(2);
-      browser.runtime.sendMessage({time: time, timing: timing});
+      var promise = browser.runtime.sendMessage({time: time, timing: timing});
+      promise.catch((reason) => console.log(reason));
     } else {
       setTimeout(startCollect, 100);
     }
