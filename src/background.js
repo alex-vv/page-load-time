87524da927a101ad0a1dd0460097433399eff179
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2020-10-26 17:08:27 +0600

    Wait for the cache before setting the popup #31

--- src/background.js
+++ src/background.js
@@ -4,10 +4,12 @@ browser.runtime.onMessage.addListener((request, sender) => {
   browser.storage.local.get('cache').then(data => {
     if (!data.cache) data.cache = {};
     data.cache['tab' + sender.tab.id] = request.timing;
-    browser.storage.local.set(data);
+    browser.storage.local.set(data).then(() => {
+      browser.browserAction.setBadgeText({text: request.time, tabId: sender.tab.id});
+      browser.browserAction.setPopup({tabId: sender.tab.id, popup: "popup.html"})
+    });
   });
-  browser.browserAction.setBadgeText({text: request.time, tabId: sender.tab.id});
-  browser.browserAction.setPopup({tabId: sender.tab.id, popup: "popup.html"})
+
 });
 
 // cache eviction
