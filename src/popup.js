13fa65694dca6fe52e32c257bc6b20d891f6e6fd
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2021-01-05 00:31:30 +0600

    Improve DOM timings, fixes #33

--- src/popup.js
+++ src/popup.js
@@ -23,9 +23,11 @@ browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
     set('connect', t.connectStart, t.connectEnd);
     set('request', t.requestStart, t.responseStart);
     set('response', t.responseStart, t.responseEnd);
-    set('dom', t.responseStart, t.domComplete);
-    set('domInteractive', t.domInteractive, t.domInteractive, true);
-    set('contentLoaded', t.domContentLoadedEventStart, t.domContentLoadedEventEnd, true);
+    set('dom', t.responseEnd, t.domComplete);
+    set('domParse', t.responseEnd, t.domInteractive);
+    set('domScripts', t.domInteractive, t.domContentLoadedEventStart);
+    set('contentLoaded', t.domContentLoadedEventStart, t.domContentLoadedEventEnd);
+    set('domSubRes', t.domContentLoadedEventEnd, t.domComplete);
     set('load', t.loadEventStart, t.loadEventEnd);
     document.getElementById("total").innerHTML = Math.round(t.duration);
     document.getElementById("requestStart").innerHTML = new Date(t.start).toString();
